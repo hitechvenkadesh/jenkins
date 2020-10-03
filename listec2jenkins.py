7c9c47b44f2571027e25cdb24c588496641db145
hitechvenkadesh <48478948+hitechvenkadesh@users.noreply.github.com>
2020-10-03 16:23:09 +0530

    Update listec2jenkins.py

--- listec2jenkins.py
+++ listec2jenkins.py
@@ -13,8 +13,8 @@ output = listec2.describe_instances()
 for printid in output['Reservations']:
     for printid2 in printid['Instances']:
 	for printid3 in printid2['Tags']:
-		print((printid2['InstanceId']),
-		      (printid2['InstanceType']),
-		      (printid2['LaunchTime']),
-		      (printid2['State']['Name']),
-		      (printid3['Value']))
+	    print((printid2['InstanceId']),
+		  (printid2['InstanceType']),
+		  (printid2['LaunchTime']),
+		  (printid2['State']['Name']),
+		  (printid3['Value']))
