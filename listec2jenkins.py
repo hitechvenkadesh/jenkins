76b5d54b33394adabb1a20c0530c005aef2d5286
hitechvenkadesh <48478948+hitechvenkadesh@users.noreply.github.com>
2020-10-03 16:16:50 +0530

    Update listec2jenkins.py

--- listec2jenkins.py
+++ listec2jenkins.py
@@ -12,7 +12,9 @@ listec2 = boto3.client('ec2',region_name=region,
 output = listec2.describe_instances()
 for printid in output['Reservations']:
     for printid2 in printid['Instances']:
-	print(printid2['InstanceId'],
-              printid2['InstanceType'],
-              printid2['LaunchTime'],
-              printid2['State']['Name'])
+	for printid3 in printid2['Tags']:
+            print((printid2['InstanceId']),
+                  (printid2['InstanceType']),
+                  (printid2['LaunchTime']),
+                  (printid2['State']['Name']),
+                  (printid3['Value']))
